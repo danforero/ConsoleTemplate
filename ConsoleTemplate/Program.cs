062f8be6899071cf8f5e380a10032ed918c2eb47
danfo <danfo@HOME-SERVER>
2022-11-21 09:41:22 -0600

    Add project files.

---
+++ ConsoleTemplate/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleTemplate.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+var host = AppStartup();
+var service = ActivatorUtilities.CreateInstance<DatabaseService>(host.Services);
+
+service.Connect();
+
+
+
+
+static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+}
+
+static IHost AppStartup()
+{
+    var builder = new ConfigurationBuilder();
+    BuildConfig(builder);
+
+    Log.Logger = new LoggerConfiguration() 
+                    .ReadFrom.Configuration(builder.Build())
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .CreateLogger();
+
+    Log.Logger.Information("Application Starting");
+
+    var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) => { 
+
+                })
+                .UseSerilog() 
+                .Build();
+
+    return host;
+}
---
+++ ConsoleTemplate/Services/DatabaseService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTemplate.Services
+{
+    public class DatabaseService : IDatabaseService
+    {
+        private readonly ILogger<DatabaseService> _log;
+        private readonly IConfiguration _config;
+        public DatabaseService(ILogger<DatabaseService> log, IConfiguration config)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void Connect()
+        {
+            var connectionString = _config.GetValue<string>("ConnectionStrings:DefaultConnection");
+            _log.LogInformation("Connection String {cs}", connectionString);
+        }
+    }
+}
